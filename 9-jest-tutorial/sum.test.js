1e1dee693234763a525f2015e0ea87ddf9308ea7
vicc23@gmail.com <vicc23@gmail.com>
2021-03-11 13:18:03 -0600

    Added Jest matchers

--- 9-jest-tutorial/sum.test.js
+++ 9-jest-tutorial/sum.test.js
@@ -4,6 +4,8 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
+/** Using Matchers **/
+
 test('two plus two is four', () => {
     expect(2 + 2).toBe(4);
 });
@@ -14,4 +16,36 @@ test('adding positive numbers is not zero', () => {
             expect(a + b).not.toBe(0);
         }
     }
+});
+
+test('object assignment', () => {
+    const data = { one: 1 };
+    data['two'] = 2;
+    expect(data).toEqual({ one: 1, two: 2 });
+});
+
+function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK');
+}
+
+const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'beer',
+  ];
+  
+  test('the shopping list has beer on it', () => {
+    expect(shoppingList).toContain('beer');
+    expect(new Set(shoppingList)).toContain('beer');
+  });
+
+test('compiling android goes as expected', () => {
+    expect(() => compileAndroidCode()).toThrow();
+    expect(() => compileAndroidCode()).toThrow(Error);
+
+    // You can also use the exact error message or a regexp
+    expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+    expect(() => compileAndroidCode()).toThrow(/JDK/);
 });
