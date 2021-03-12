ac9c8785289cb89cbf03e8eae5fc4472f799c529
vicc23@gmail.com <vicc23@gmail.com>
2021-03-11 21:13:40 -0600

    Refactor array analysis

--- 10-testing-practice/code/5-array-analysis.js
+++ 10-testing-practice/code/5-array-analysis.js
@@ -1,33 +1,10 @@
-function average(arr) {
-    const sum = arr.reduce((acc, val) => {
-        return acc + val; //?
-    });
-    return sum / arr.length; //?
-}
-
-function getMin(arr) {
-    return arr.reduce((prev, curr) => {
-        return prev < curr ? prev : curr;
-    });
-}
-
-function getMax(arr) {
-    return arr.reduce((prev, curr) => {
-        return prev > curr ? prev : curr;
-    });
-}
-
-function getLength(arr) {
-    return arr.length;
-}
-
 function analize(arr) {
 
     return ({
-        average: average(arr),
-        min: getMin(arr),
-        max: getMax(arr),
-        length: getLength(arr)
+        average: arr.reduce((acc, val) => { return acc + val; })/arr.length,
+        min: arr.reduce((prev, curr) => { return prev < curr ? prev : curr; }),
+        max: arr.reduce((prev, curr) => { return prev > curr ? prev : curr; }),
+        length: arr.length
     })
 
 }
