0d2858cfdb53591110ad7f51820a735a63f51cdb
Victor Cruz <vicc23@gmail.com>
2021-05-14 20:53:54 -0500

    Nodejs modules

---
+++ 12-nodejs/myDate.js
@@ -0,0 +1,5 @@
+'use strict'
+
+exports.myDateTime = function () {
+    return Date();
+};
---
+++ 12-nodejs/myfirst.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var http = require('http');
+var dt = require('./myDate');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
+}).listen(8080);
